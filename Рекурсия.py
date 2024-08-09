2b66a4d382753cdc491521f35dd087ab3c8c8892
DanteDisward <94927935+DanteDisward@users.noreply.github.com>
2024-08-09 11:50:09 +0500

    Update Рекурсия.py
    Преподавателю не понравилось, что передал в функцию другое число, не то которое было в примере решения

--- Рекурсия.py
+++ Рекурсия.py
@@ -8,5 +8,5 @@ def get_multiplied_digits(number):
     return first * get_multiplied_digits(int(str_number[1:]))
 
 
-result = get_multiplied_digits(4000052000060000)
+result = get_multiplied_digits(40203)
 print(result)
